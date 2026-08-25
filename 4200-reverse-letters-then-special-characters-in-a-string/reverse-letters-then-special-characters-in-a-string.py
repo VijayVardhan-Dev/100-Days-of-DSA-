53397271e4cd9819ff74f5cd5ec74e8a78076e6c
Vardhan Keerthi <vardhankeerthi.dev@gmail.com>
2026-08-25 21:51:13 +0530

    Time: 3 ms (64.22%) | Memory: 19.2 MB (79.58%) - LeetSync

--- 4200-reverse-letters-then-special-characters-in-a-string/reverse-letters-then-special-characters-in-a-string.py
+++ 4200-reverse-letters-then-special-characters-in-a-string/reverse-letters-then-special-characters-in-a-string.py
@@ -1,46 +1,28 @@
 class Solution:
-
     def reverseByType(self, s: str) -> str:
-
-        s = list(s)
-
         N = len(s)
-
+        s = list(s)
         l = 0
-        r = N - 1
-
+        r = N-1
         while l < r:
-
-            if s[l].isalpha():
-
-                while r > l and not s[r].isalpha():
-                    r -= 1
-
-                if l < r:
-                    s[l], s[r] = s[r], s[l]
-                    l += 1
-                    r -= 1
-
-            else:
-                while l < r and not s[l].isalpha():
-                    l += 1
-
-        l = 0
-        r = N - 1
-
-        while l < r:
-
-            if not s[l].isalnum():
-
-                while r > l and s[r].isalnum():
-                    r -= 1
-
-                if l < r:
-                    s[l], s[r] = s[r], s[l]
-                    l += 1
-                    r -= 1
-
-            else:
+            while l < r and not s[l].isalnum():
                 l += 1
-
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if l < r:
+                s[l],s[r] = s[r],s[l]
+                l += 1
+                r -= 1
+        l = 0
+        r = N-1
+        while l < r:
+            while l < r and  s[l].isalnum():
+                l += 1
+            while l < r and  s[r].isalnum():
+                r -= 1
+            if l < r:
+                s[l],s[r] = s[r],s[l]
+                l += 1
+                r -= 1
         return "".join(s)
+        
