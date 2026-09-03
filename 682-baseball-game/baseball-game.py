63cdce15a75bd58a1d3eb36ed2b00f93c2f1c0d8
Vardhan Keerthi <vardhankeerthi.dev@gmail.com>
2026-09-03 21:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.88%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -2,29 +2,24 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         stack = []
         stack_sum = 0
-        def is_integer(s):
-            try:
-                int(s)
-                return True
-            except ValueError:
-                return False
             
         for i in operations:
-            if is_integer(i):
-                val = int(i)
-                stack.append(val)
-                stack_sum += val
-            elif i == '+':
+            if i == '+':
                 val = stack[-1] + stack[-2]
                 stack.append(val)
                 stack_sum += val
             elif i == 'C':
                 val = stack.pop()
                 stack_sum -= val
-            else:
+            elif i == 'D':
                 val = stack[-1] * 2
                 stack.append(val)
                 stack_sum += val
+            else:
+                val = int(i)
+                stack.append(val)
+                stack_sum += val
+
         return stack_sum
 
 
