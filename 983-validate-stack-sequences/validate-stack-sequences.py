96f89e82c43fa4d4d94b86a737062d08f12173be
Vardhan Keerthi <vardhankeerthi.dev@gmail.com>
2026-09-13 00:19:11 +0530

    Time: 4 ms (14.21%) | Memory: 19.2 MB (97.56%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+        l = 0
+        for i in range(len(pushed)):
+            stack.append(pushed[i])
+            while stack and stack[-1] == popped[l]:
+                stack.pop()
+                l += 1
+                
+        if stack:
+            return False
+        return True
+            
+        
